8803ed8f277e4c2b63cc5fc486e6b46d78f4acd3
Rafael <Rafabatera-8@hotmail.com>
2021-09-08 23:44:11 -0300

    configurações do projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,4 @@
+import './styles.scss';
+export const Header = (): JSX.Element => {
+  return <div>Olá, aqui é o header</div>;
+};
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from '../../components/Header';
+import './styles.scss';
+
+export const Home = (): JSX.Element => {
+  return (
+    <div className="Header">
+      <Header />
+    </div>
+  );
+};
